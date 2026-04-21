b740fd76091f513267d9e6cf4fbb2efc218b752e
pavelVPo <97514993+pavelVPo@users.noreply.github.com>
2026-04-21 13:16:27 +0300

    Add files via upload

---
+++ 4-1-ext__prepareSDF_labels_additions_deailed__Rcode.R
@@ -0,0 +1,97 @@
+library(tidyverse)
+# Some modifications:
+# IF SWALLOWED: 	-> IF SWALLOWED,
+# IF ON SKIN:   	-> IF ON SKIN,
+# IF INHALED:   	-> IF INHALED,
+# IF IN EYES:   	-> IF IN EYES,
+# IF ON CLOTHING:	-> IF ON CLOTHING,
+# concerned: 		-> concerned,
+# occurs: 			-> occurs,
+# persists:			-> persists,
+# symptoms: 		-> symptoms,
+# fire:				-> fire,
+# quantities: 		-> quantities,
+# 50 ?C/122?F. 		-> 50 C
+#  ?C?F.			-> ""
+# ):				-> ),
+
+# Input
+# Compounds
+sdf_raw <- read_file("C:/.../data/PubChem_compound_cache_rukLgqoMz7D4nkeHxf8OoA5QnDBosCu-UZsw8kqKIvNKkx4_records_SD.SDF") |>
+				#str_replace_all("\n", "\r\n") |>
+				str_trim() |>
+				as_tibble() |>
+				separate_longer_delim(value, delim = "$$$$") |>
+				mutate(value = str_trim(value)) |>
+				filter(value != "") |>
+				separate_wider_delim(value, delim = "\r\n> <PUBCHEM_COMPOUND_CID>", names=c("mol", "data"), too_few = "align_start") |>
+				separate_wider_delim(data, delim = "\r\n> <PUBCHEM_COMPOUND_CANONICALIZED>", names=c("id", "data"), too_few = "align_start") |>
+				separate_wider_delim(data, delim = "\r\n>  <MNA_DESCRIPTORS>", names=c("data", "mna"), too_few = "align_start") |>
+				mutate(mna_cid = str_trim(id), mna = str_trim(mna), mol = str_trim(mol)) |>
+				select(-data) |>
+				group_by(mna) |>
+				mutate(all_cid = str_c(mna_cid, collapse = ", ")) |>
+				slice_head(n = 1) |>
+				ungroup()
+# Prepare IDs
+mna_cids <- sdf_raw |> select(mna_cid, all_cid) |>
+					   separate_longer_delim(all_cid, ", ") |>
+					   mutate(all_cid = str_trim(all_cid)) |>
+					   distinct()
+# Read the data
+data_raw <- read_tsv("C:/.../data/env_precautions_cid.tsv") |>
+					mutate(cid = as.character(cid)) |>
+					select(-hnid) |>
+					mutate(warning = str_replace(warning, coll("SWALLOWED:"), "IF SWALLOWED,")) |>
+					mutate(warning = str_replace(warning, coll("SKIN:"), "SKIN,")) |>
+					mutate(warning = str_replace(warning, coll("INHALED:"), "INHALED,")) |>
+					mutate(warning = str_replace(warning, coll("EYES:"), "EYES,")) |>
+					mutate(warning = str_replace(warning, coll("CLOTHING:"), "CLOTHING,")) |>
+					mutate(warning = str_replace(warning, coll("concerned:"), "concerned,")) |>
+					mutate(warning = str_replace(warning, coll("occurs:"), "occurs,")) |>
+					mutate(warning = str_replace(warning, coll("persists:"), "persists,")) |>
+					mutate(warning = str_replace(warning, coll("symptoms:"), "symptoms,")) |>
+					mutate(warning = str_replace(warning, coll("fire:"), "fire,")) |>
+					mutate(warning = str_replace(warning, coll("quantities:"), "quantities,")) |>
+					mutate(warning = str_replace(warning, coll("):"), "),")) |>
+					mutate(warning = str_replace(warning, coll("50 ?C/122?F"), "50 C")) |>
+					mutate(warning = str_replace(warning, coll("?C?F"), "")) |> 
+					separate_wider_delim(warning, delim = ": ", names = c("code", "label")) |>
+					inner_join(mna_cids, by = c("cid" = "all_cid")) |>
+					select(mna_cid, code, label)
+# Summary
+code_sum 	<- data_raw |> group_by(code) |> summarize(n = n())
+label_sum 	<- data_raw |> group_by(label) |> summarize(n = n())
+
+## 11.02.26: Make sure that each code in combined label (like H303+H313+H333) is also included as a distinct label (like H303, H313, H333)
+# Get labels' descriptions
+label_descr <- data_raw |> select(code, label) |> distinct()
+# Separate combined labels
+data_separated <- data_raw |> select(-label) |> separate_longer_delim(code, delim = "+")
+# Re-introduce combined labels
+data_main <- bind_rows(data_separated, data_raw |> select(-label)) |> distinct() |> inner_join(label_descr)
+# Leave H-codes only
+data_h <- data_main |> filter(str_starts(code, "H"))
+
+# Prepare the data
+# Codes and all
+data <- data_h |> group_by(mna_cid) |>
+						mutate(code = str_c(code, collapse = "\r\n")) |>
+						mutate(label = str_c(label, collapse = "\r\n")) |>
+						slice_head(n = 1) |>
+						ungroup()
+
+# Add mols
+sdf_proc     <- sdf_raw |> inner_join(data)
+
+## Prepare SDFs
+# Codes and labels
+sdf <- sdf_proc |> mutate(id_rec = "\r\n>  <CID>\r\n", allid_rec = "\r\n\r\n>  <CID_all>\r\n",
+							ghscode_rec = "\r\n\r\n>  <GHS code>\r\n", ghslabel_rec = "\r\n\r\n>  <GHS label>\r\n",
+							end_rec = "\r\n\r\n$$$$") |>
+					select(mol, id_rec, mna_cid, allid_rec, all_cid, ghscode_rec, code, ghslabel_rec, label,
+							 end_rec) |>
+							 unite("record", mol:end_rec, sep = "")
+
+## Export SDFs
+write_lines(str_c("", sdf[[1]]), "C:/.../data/sdfs/hazard_labels_PC_CIDP_NLM__ghs_extended_detailed.SDF")
